5bda93f42cc5b5d129497539406b7b7cf3b41f06
Iuri <barberiuri@gmail.com>
2026-05-19 16:52:19 -0300

    board: surfer-c3-1.14tft: use NoAudioCodec instead of ES8311

--- main/boards/surfer-c3-1.14tft/surfer-c3-1.14tft.cc
+++ main/boards/surfer-c3-1.14tft/surfer-c3-1.14tft.cc
@@ -1,15 +1,13 @@
 #include "wifi_board.h"
-#include "codecs/es8311_audio_codec.h"
+#include "codecs/no_audio_codec.h"
 #include "display/lcd_display.h"
 #include "application.h"
 #include "button.h"
 #include "config.h"
-#include "i2c_device.h"
 
 #include <esp_log.h>
 #include <esp_efuse_table.h>
 #include <esp_lcd_panel_vendor.h>
-#include <driver/i2c_master.h>
 #include <driver/spi_common.h>
 #include <esp_wifi.h>
 
@@ -21,38 +19,12 @@
 
 class SurferC3114TFT : public WifiBoard {
 private:
-    i2c_master_bus_handle_t codec_i2c_bus_;
     Button boot_button_;
     LcdDisplay* display_;
     PowerSaveTimer* power_save_timer_;
     PowerManager* power_manager_;
     esp_lcd_panel_io_handle_t panel_io_ = nullptr;
     esp_lcd_panel_handle_t panel_ = nullptr;
-
-    void InitializeI2c() {
-        // Initialize I2C peripheral
-        i2c_master_bus_config_t i2c_bus_cfg = {
-            .i2c_port = I2C_NUM_0,
-            .sda_io_num = AUDIO_CODEC_I2C_SDA_PIN,
-            .scl_io_num = AUDIO_CODEC_I2C_SCL_PIN,
-            .clk_source = I2C_CLK_SRC_DEFAULT,
-            .glitch_ignore_cnt = 7,
-            .intr_priority = 0,
-            .trans_queue_depth = 0,
-            .flags = {
-                .enable_internal_pullup = 1,
-            },
-        };
-        ESP_ERROR_CHECK(i2c_new_master_bus(&i2c_bus_cfg, &codec_i2c_bus_));
-
-        // Print I2C bus info
-        if (i2c_master_probe(codec_i2c_bus_, 0x18, 1000) != ESP_OK) {
-            while (true) {
-                ESP_LOGE(TAG, "Failed to probe I2C bus, please check if you have installed the correct firmware");
-                vTaskDelay(1000 / portTICK_PERIOD_MS);
-            }
-        }
-    }
 
     void InitializePowerManager() {
         power_manager_ = new PowerManager(GPIO_NUM_NC);
@@ -150,7 +122,6 @@ public:
         InitializePowerManager();
         InitializePowerSaveTimer(); 
 
-        InitializeI2c();
         InitializeSpi();
         InitializeSt7789Display();
         InitializeButtons();
@@ -162,18 +133,13 @@ public:
     }
 
     virtual AudioCodec* GetAudioCodec() override {
-        static Es8311AudioCodec audio_codec(
-            codec_i2c_bus_, 
-            I2C_NUM_0, 
-            AUDIO_INPUT_SAMPLE_RATE, 
+        static NoAudioCodecDuplex audio_codec(
+            AUDIO_INPUT_SAMPLE_RATE,
             AUDIO_OUTPUT_SAMPLE_RATE,
-            AUDIO_I2S_GPIO_MCLK, 
-            AUDIO_I2S_GPIO_BCLK, 
-            AUDIO_I2S_GPIO_WS, 
-            AUDIO_I2S_GPIO_DOUT, 
-            AUDIO_I2S_GPIO_DIN,
-            AUDIO_CODEC_PA_PIN, 
-            AUDIO_CODEC_ES8311_ADDR);
+            AUDIO_I2S_GPIO_BCLK,
+            AUDIO_I2S_GPIO_WS,
+            AUDIO_I2S_GPIO_DOUT,
+            AUDIO_I2S_GPIO_DIN);
         return &audio_codec;
     }
 
